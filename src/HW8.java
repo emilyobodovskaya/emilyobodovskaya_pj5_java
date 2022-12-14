e20148289c9710ccd0dcf10c43261f9820dc0a58
emily <obodovskaya.m@gmail.com>
2022-12-14 14:28:09 +0200

    Added hw7.

---
+++ src/HW8.java
@@ -0,0 +1,57 @@
+public class HW8 {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 3; i++){
+            System.out.println( i + "-space shuttle" ); }
+       for(int i = 5; i <= 8; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 10; i <= 13; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 15; i <= 18; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 20; i <= 23; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 25; i <= 28; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 30; i <= 33; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 35; i <= 38; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 50; i <= 53; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 55; i <= 58; i++){
+        System.out.println( i + "-space shuttle" );
+    }
+        for(int i = 60; i <= 63; i++){
+        System.out.println( i + "-space shuttle" );
+    }
+        for(int i = 65; i <= 68; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 70; i <= 73; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 75; i <= 78; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 80; i <= 83; i++){
+        System.out.println( i + "-space shuttle" );
+    }
+        for(int i = 85; i <= 88; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+        for(int i = 100; i <= 100; i++){
+            System.out.println( i + "-space shuttle" );
+        }
+
+
+
+    }
+}
