17e7e45e13d94c6ba6b6f3184cadbf40cacc9081
emily <obodovskaya.m@gmail.com>
2022-12-14 14:48:59 +0200

    Added hw

---
+++ src/HW.java
@@ -0,0 +1,27 @@
+public class HW {
+    public static void main(String[] args) {
+        for(int i = 0; i < 39; i++)
+      if ( i != 4  )
+          if ( i != 9 )
+             if ( i != 14 )
+                if ( i != 19 )
+                  if ( i != 24 )
+                     if ( i != 29 )
+                       if ( i != 34 ) {
+
+                System.out.println( i + "-space shuttle" );
+        }
+    for(int i = 50; i < 89; i++)
+            if ( i != 54  )
+               if ( i != 59 )
+                  if ( i != 64 )
+                     if ( i != 69 )
+                       if ( i != 74 )
+                          if ( i != 79 )
+                              if ( i != 84 ) {
+
+
+                         System.out.println( i + "-space shuttle" );
+                                    }
+             }
+}
